aa43aa2a7475838fa34a25a11db99159bc509593
lucas <gepeoverload@gmail.com>
2022-08-17 09:42:02 +0200

    added server folder and server logic

---
+++ server/config.js
@@ -0,0 +1,12 @@
+const config = {
+  db: {
+    //TODO:HIDE THESE
+
+    host: "localhost",
+    user: "becode",
+    password: "becode",
+    database: "languages",
+  },
+  listPerPage: 10,
+};
+module.exports = config;
