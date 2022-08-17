977a254fe597fd021218a016bbefe9ead5c50ce2
Jelle VdP <jellevandepoel@gmail.com>
2022-08-17 19:57:02 +0200

    changed dropshadow & added hovereffect on product overview

--- app/src/pages/HomePage.jsx
+++ app/src/pages/HomePage.jsx
@@ -11,6 +11,7 @@ function HomePage() {
         <div className="container">
             <h1>HOME</h1>
             <section className="products-wrapper">
+                {/* <img src={require("../assets/images/gbay-logo-yellow.svg").default} alt="G-bay logo" /> */}
                 {products.map((product) => (
                     <article key={crypto.randomUUID()} className="product">
                         <img className="product__img" src={product.url} />
