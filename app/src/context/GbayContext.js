0780e66ca1d7f29900ddb7b83e29436aca0d64c7
shivamtg <shivamtg1376@gmail.com>
2022-08-19 15:41:12 +0200

    add shopping cart page basic structure

--- app/src/context/GbayContext.js
+++ app/src/context/GbayContext.js
@@ -53,6 +53,10 @@ export const GbayProvider = ({ children }) => {
             user_id: 1,
             shortDescription: "Multiple screens with OG Putin",
             longDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ligula sem, pellentesque et elementum non, luctus sit amet arcu. Duis faucibus, ipsum vel interdum faucibus, nunc velit porta magna, vitae pellentesque purus urna vitae libero. Proin mattis lorem non nisi dictum facilisis. Morbi et nibh turpis. Nunc ultricies nulla diam, vel bibendum justo vehicula quis. Donec ut felis id dolor faucibus placerat. Vivamus facilisis ligula in scelerisque dapibus. Duis sagittis consequat pulvinar. Phasellus pharetra luctus nulla, ut dictum lacus eleifend consectetur. Duis in molestie mauris. Quisque auctor dolor et arcu molestie, ornare aliquam lorem pulvinar. Nam mattis est at nulla tincidunt semper. Aenean nec facilisis nibh, a elementum urna."
+        },
+        {
+            id: 6,
+            name: "Product 6"
         }
     ]);
 
@@ -63,6 +67,10 @@ export const GbayProvider = ({ children }) => {
             password: "becode"
         }
     ]);
+
+    const [cart , setCart] = useState([
+
+    ])
 
     const [loggedInUser, setLoggedInUser] = useState(null);
 
---
+++ app/src/pages/ShoppingCartPage.jsx
@@ -0,0 +1,13 @@
+import {useContext, useEffect} from 'react';
+import GbayContext from '../context/GbayContext';
+import { useNavigate } from 'react-router-dom';
+import React, {useState} from 'react';
+import './ShoppingCartPage.scss';
+
+function ShoppingCartPage(){
+    return(
+        <h1>Your Cart</h1>
+    )
+}
+
+export default ShoppingCartPage;
