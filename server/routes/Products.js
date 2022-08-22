a6fa6d06c38c27f349dec5993c3dec04a31bef18
lucas <gepeoverload@gmail.com>
2022-08-22 17:27:03 +0200

    updated product routes

--- server/routes/Products.js
+++ server/routes/Products.js
@@ -4,41 +4,41 @@ const products = require("../services/Products");
 
 /* GET products. */
 router.get("/", async function (req, res, next) {
-    try {
-        res.json(await products.getProducts(req.query.page));
-    } catch (err) {
-        console.log(err);
-        next(err);
-    }
+  try {
+    res.json(await products.getProducts(req.query.page));
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
 });
 
 /* POST product */
 router.post("/", async function (req, res, next) {
-    try {
-        res.json(await products.createProduct(req.body));
-    } catch (err) {
-        console.error(`Error while creating product`, err.message);
-        next(err);
-    }
+  try {
+    res.json(await products.createProduct(req.body));
+  } catch (err) {
+    console.error(`Error while creating product`, err.message);
+    next(err);
+  }
 });
 
-/* PUT programming language */
+/* PUT product */
 router.put("/:id", async function (req, res, next) {
-    try {
-        res.json(await products.updateProduct(req.params.id, req.body));
-    } catch (err) {
-        console.error(`Error while updating product`, err.message);
-        next(err);
-    }
+  try {
+    res.json(await products.updateProduct(req.params.id, req.body));
+  } catch (err) {
+    console.error(`Error while updating product`, err.message);
+    next(err);
+  }
 });
-/* DELETE programming language */
+/* DELETE product */
 router.delete("/:id", async function (req, res, next) {
-    try {
-        res.json(await products.removeProduct(req.params.id));
-    } catch (err) {
-        console.error(`Error while deleting product`, err.message);
-        next(err);
-    }
+  try {
+    res.json(await products.removeProduct(req.params.id));
+  } catch (err) {
+    console.error(`Error while deleting product`, err.message);
+    next(err);
+  }
 });
 
 module.exports = router;
