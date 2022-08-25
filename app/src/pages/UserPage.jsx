01331df3de82a48151932c5e99272ccc0d5fd032
shivamtg <shivamtg1376@gmail.com>
2022-08-25 16:26:42 +0200

    infinite fetch issue solved

--- app/src/pages/UserPage.jsx
+++ app/src/pages/UserPage.jsx
@@ -1,7 +1,7 @@
-import { useContext } from 'react';
+import {useContext, useEffect} from 'react';
 import GbayContext from '../context/GbayContext';
-import { Link } from "react-router-dom";
-import { IoMdAddCircleOutline } from "react-icons/io";
+import {Link} from "react-router-dom";
+import {IoMdAddCircleOutline} from "react-icons/io";
 
 import "./UserPage.scss";
 
@@ -9,38 +9,37 @@ import ProductItem from '../components/ProductItem';
 
 function UserPage() {
 
-    const { products, loggedInUser, userProducts, setUserProducts } = useContext(GbayContext);
+    const {products, loggedInUser, userProducts, setUserProducts} = useContext(GbayContext);
 
-    fetch("http://localhost:9000/products/byuser/" + loggedInUser.id)
-        .then(res => res.json())
-        .then(data => {
-            console.log("producten", data);
-            setUserProducts(data);
-        })
-        .catch(err => console.log(err));
+    useEffect(() => {
+        fetch("http://localhost:9000/products/byuser/" + 1 /*loggedInUser.id*/)
+            .then(res => res.json())
+            .then(data => {
+                console.log("product", data.resultData);
+                setUserProducts(data.resultData);
+            })
+            .catch(err => console.log(err));
+    }, [])
+
 
     return (
         <div className="container user-page">
             <h1>Your profile</h1>
-            <p className="user__welcome">Welcome <b>{loggedInUser.username}</b></p>
+            <p className="user__welcome">Welcome <b>{/*loggedInUser.username*/}</b></p>
             <section>
                 <h3>Add a new product</h3>
                 <Link className="user__add-new-product" to="./add-product">
-                    <IoMdAddCircleOutline />
+                    <IoMdAddCircleOutline/>
                 </Link>
             </section>
             <section>
                 <h3>These are your current listings:</h3>
                 <section className="products-wrapper">
-                    {products.map((product) => {
-                        if (product.user_id === loggedInUser.id) {
-                            return (
-                                <div className="user__current-listing">
-                                    <ProductItem product={product} hoverable={false} rud={true} />
-                                </div>
-                            )
-                        }
-                    })}
+                    {userProducts.map((product) =>
+                        <div className="user__current-listing" key={crypto.randomUUID()}>
+                            <ProductItem product={product} hoverable={false} rud={true}/>
+                        </div>
+                    )}
                 </section>
             </section>
         </div>
