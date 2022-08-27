c25607948f2eb86ad9b8adc700fb5cdccbbcbf0c
Besart Elezi <besart.elezi@hotmail.com>
2022-08-27 10:53:08 +0200

    added base file structure
    also added buyer information

---
+++ app/src/pages/PurchaseConfirmation.jsx
@@ -0,0 +1,110 @@
+import {useContext, useEffect} from 'react';
+import GbayContext from '../context/GbayContext';
+import {useNavigate} from 'react-router-dom';
+import React, {useState} from 'react';
+
+function PurchaseConfirmation () {
+
+    const {cart, setCart, loggedInUser} = useContext(GbayContext);
+
+    const navigate = useNavigate();
+
+    //users full address, it is being split up to make it look better on the site
+    let originalAddress = loggedInUser.address;
+    const addressArray = originalAddress.split(",");
+
+    //delivery address data in variables
+    let street = addressArray[0];
+    let streetNumber = addressArray[1];
+    let city = addressArray[2];
+    let zipcode = addressArray[3];
+    let usersFullName = loggedInUser.firstname + " " + loggedInUser.lastname
+
+    //setting delivery date
+    let today = new Date();
+    let deliveryDate = new Date(today);
+    deliveryDate.setDate(deliveryDate.getDate() + 3);
+
+    //setting delivery date into separate variables
+    const day = String(deliveryDate.getDate()).padStart(2, '0');
+    const month = String(deliveryDate.getMonth() + 1).padStart(2, '0');
+    const year = deliveryDate.getFullYear();
+
+    //setting delivery date into desired layout
+    const deliveryDateLayout = day + "/" + month + "/" + year;
+
+    //emails
+    let buyerEmail = loggedInUser.email
+    let sellerEmail
+
+    //cals all functions necessary for the purchase functionality
+    const handlePurchaseConfirmation = () => {
+        //first, the email functionality needs to be called
+        confirmationEmail()
+
+        //then, the selected product needs to be removed from the list
+
+        //lastly, the user gets a confirmation (popup?) that will send them back to the homepage
+        //the confirmation must also say that they should have received an email
+
+        goToHomepage();
+    }
+
+    //____________________________________  Email functionality  ______________________________________
+
+    const confirmationEmail = () => {
+        console.log("email has been sent");
+        //an email must be sent to the buyer
+        //an email must be sent to the seller
+    }
+
+    //_________________________________________________________________________________________________
+
+    //on button click, user gets sent back to the cart
+    const goToCart = () => {
+        navigate('/shopping-cart')
+    }
+
+    const goToHomepage = () => {
+        navigate('/');
+    }
+
+    return (
+        <div className="container PurchaseConfirmation">
+            <div className ="deliveryAddress">
+
+                <h1>Delivery address:</h1>
+                <p>{usersFullName}</p>
+                <p>{street} {streetNumber}</p>
+                <p>{city} {zipcode}</p>
+
+            </div>
+
+            <div className="purchasedProducts">
+
+                <h1>Delivery moment:</h1>
+
+                {/*<p>{cart.product.price}</p>*/}
+
+                <p>{deliveryDateLayout}</p>
+
+            </div>
+
+            <div className="paymentMethod">
+
+                <h1>Payment method:</h1>
+
+            </div>
+
+            <div className="buttonContainer">
+
+                <button className="btn--cta" onClick={handlePurchaseConfirmation}>Confirm</button>
+                <button className="btn--cta" onClick={goToCart}>I reject my purchase, JOJO!</button>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default PurchaseConfirmation
